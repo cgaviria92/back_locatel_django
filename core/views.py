66dc041fae59cc0bdd234b87b6b9fd9a4b396b41
carlos gaviria <ing.carlos.gaviria@gmail.com>
2024-06-23 11:42:35 -0500

    se modifica de api restful a solo Get y Post

--- core/views.py
+++ core/views.py
@@ -1,33 +1,74 @@
-from rest_framework import viewsets, status,filters
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Cliente, Producto, CabeceraVenta, DetalleVenta
 from .serializers import ClienteSerializer, ProductoSerializer, CabeceraVentaSerializer, DetalleVentaSerializer
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
-class ClienteViewSet(viewsets.ModelViewSet):
-    queryset = Cliente.objects.all()
-    serializer_class = ClienteSerializer
+class ClienteViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
-class ProductoViewSet(viewsets.ModelViewSet):
-    queryset = Producto.objects.all()
-    serializer_class = ProductoSerializer
+    def list(self, request, *args, **kwargs):
+        queryset = Cliente.objects.all()
+        serializer = ClienteSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = ClienteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProductoViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
-class CabeceraVentaViewSet(viewsets.ModelViewSet):
-    queryset = CabeceraVenta.objects.all()
-    serializer_class = CabeceraVentaSerializer
+    def list(self, request, *args, **kwargs):
+        queryset = Producto.objects.all()
+        serializer = ProductoSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = ProductoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CabeceraVentaViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ['fecha']
     ordering_fields = ['fecha']
     ordering = ['fecha']
+
+    def filter_queryset(self, queryset):
+        for backend in list(self.filter_backends):
+            queryset = backend().filter_queryset(self.request, queryset, self)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = CabeceraVenta.objects.all()
+        queryset = self.filter_queryset(queryset)
+        serializer = CabeceraVentaSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = CabeceraVentaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+class DetalleVentaViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
-class DetalleVentaViewSet(viewsets.ModelViewSet):
-    queryset = DetalleVenta.objects.all()
-    serializer_class = DetalleVentaSerializer
-    permission_classes = [IsAuthenticated]
+    def list(self, request, *args, **kwargs):
+        queryset = DetalleVenta.objects.all()
+        serializer = DetalleVentaSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -41,10 +82,12 @@ class DetalleVentaViewSet(viewsets.ModelViewSet):
             return Response({"error": "cabecera_venta not found"}, status=status.HTTP_404_NOT_FOUND)
         
         data['cabecera_venta'] = cabecera_venta.id
-        serializer = self.get_serializer(data=data)
+        serializer = DetalleVentaSerializer(data=data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+
+#permission_classes = [IsAuthenticated]
+#from rest_framework.permissions import IsAuthenticated
