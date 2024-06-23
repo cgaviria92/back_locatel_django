79297990e5673b83941641b6e034031a8f1aa069
carlos gaviria <ing.carlos.gaviria@gmail.com>
2024-06-23 11:41:17 -0500

    se agrega basename

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,10 @@ from rest_framework.routers import DefaultRouter
 from .views import ClienteViewSet, ProductoViewSet, CabeceraVentaViewSet, DetalleVentaViewSet
 
 router = DefaultRouter()
-router.register(r'clientes', ClienteViewSet)
-router.register(r'productos', ProductoViewSet)
-router.register(r'cabecera-ventas', CabeceraVentaViewSet)
-router.register(r'detalle-ventas', DetalleVentaViewSet)
+router.register(r'clientes', ClienteViewSet, basename='cliente')
+router.register(r'productos', ProductoViewSet, basename='producto')
+router.register(r'cabecera-ventas', CabeceraVentaViewSet, basename='cabeceraventa')
+router.register(r'detalle-ventas', DetalleVentaViewSet, basename='detalleventa')
 
 urlpatterns = [
     path('', include(router.urls)),
