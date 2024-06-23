4aea825e2ead60fb0cbd15010ecc9d6f4abf7a3e
carlos gaviria <ing.carlos.gaviria@gmail.com>
2024-06-23 11:04:33 -0500

    se comenta soap

--- ventas/urls.py
+++ ventas/urls.py
@@ -40,7 +40,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('soap/', include('core.soap.urls_soap')),
+    #path('soap/', include('core.soap.urls_soap')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
